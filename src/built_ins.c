aaf28ad475289df1ed139da16809bf50bc418c52
Christian Camargo Del Moro <christian.delmor@gmail.com>
2023-01-09 00:35:09 -0300

    Export built-in done.

--- src/built_ins.c
+++ src/built_ins.c
@@ -6,7 +6,7 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 18:10:47 by ccamargo          #+#    #+#             */
-/*   Updated: 2023/01/07 16:12:49 by ccamargo         ###   ########.fr       */
+/*   Updated: 2023/01/09 00:33:10 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@ void	check_built_in(t_cmd *cmd, t_shell *shell)
 	t_list	*tmp_content;
 
 	tmp_content = cmd->cmd_table->content;
-	if (!ft_strncmp((char *) tmp_content->content, "echo", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "echo", \
+	ft_strlen((char *) tmp_content->content)))
 		run_echo(cmd);
-	if (!ft_strncmp((char *) tmp_content->content, "cd", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "cd", \
+	ft_strlen((char *) tmp_content->content)))
 		run_cd(cmd, shell);
-	if (!ft_strncmp((char *) tmp_content->content, "pwd", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "pwd", \
+	ft_strlen((char *) tmp_content->content)))
 		run_pwd(shell);
-	if (!ft_strncmp((char *) tmp_content->content, "export", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "export", \
+	ft_strlen((char *) tmp_content->content)))
 		run_export(cmd, shell);
-	if (!ft_strncmp((char *) tmp_content->content, "unset", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "unset", \
+	ft_strlen((char *) tmp_content->content)))
 		run_unset(cmd, shell);
-	if (!ft_strncmp((char *) tmp_content->content, "env", ft_strlen((char *) tmp_content->content)))
+	if (!ft_strncmp((char *) tmp_content->content, "env", \
+	ft_strlen((char *) tmp_content->content)))
 		run_env(shell);
 }
--- src/export_built_in.c
+++ src/export_built_in.c
@@ -6,20 +6,58 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 16:12:37 by ccamargo          #+#    #+#             */
-/*   Updated: 2023/01/07 16:13:37 by ccamargo         ###   ########.fr       */
+/*   Updated: 2023/01/09 00:29:55 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	run_export(t_cmd *cmd, t_shell *shell)
+static void	no_args(t_shell *shell)
 {
-	t_list	*tmp_content;
+	int	i;
+
+	i = 0;
+	while (shell->envp[i])
+	{
+		printf("declare -x ");
+		printf("%s\n", shell->envp[i]);
+		i++;
+	}
+}
+
+static int	is_arg_valid(t_cmd *cmd, char *arg)
+{
+	int	equals;
+	int	i;
+
+	equals = 0;
+	i = 0;
+	while (arg[i])
+	{
+		if (!ft_isalnum(arg[i]) && arg[i] != '=')
+		{
+			ft_putstr_fd("minishell: export: '", 2);
+			ft_putchar_fd(arg[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			cmd->error = 1;
+			return (0);
+		}
+		if (arg[i] == '=')
+			equals++;
+		i++;
+	}
+	if (equals > 0)
+		return (1);
+	else
+		return (0);
+}
+
+static void	realloc_env(t_shell *shell, char *env_line)
+{
 	char	**tmp_env;
 	int		line_count;
 	int		i;
 
-	tmp_content = cmd->cmd_table->content;
 	line_count = 0;
 	i = 0;
 	while (shell->envp[line_count])
@@ -31,8 +69,7 @@ void	run_export(t_cmd *cmd, t_shell *shell)
 		i++;
 		line_count--;
 	}
-	tmp_content = tmp_content->next;
-	tmp_env[i] = ft_strdup((char *) tmp_content->content);
+	tmp_env[i] = ft_strdup(env_line);
 	i = 0;
 	while (shell->envp[i])
 	{
@@ -41,4 +78,25 @@ void	run_export(t_cmd *cmd, t_shell *shell)
 	}
 	free(shell->envp);
 	shell->envp = tmp_env;
+}
+
+void	run_export(t_cmd *cmd, t_shell *shell)
+{
+	t_list	*tmp_content;
+
+	tmp_content = cmd->cmd_table->content;
+	tmp_content = tmp_content->next;
+	if (cmd->argc == 1)
+	{
+		no_args(shell);
+		return ;
+	}
+	while (tmp_content)
+	{
+		if (is_arg_valid(cmd, (char *) tmp_content->content))
+		{
+			realloc_env(shell, (char *) tmp_content->content);
+		}
+		tmp_content = tmp_content->next;
+	}
 }
